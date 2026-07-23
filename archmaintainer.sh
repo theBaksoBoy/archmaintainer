686e61d337463617fc5f3f97dbff9915acd8e507
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-07-23 18:02:15 +0200

    changed echoing messages and ordering

--- archmaintainer.sh
+++ archmaintainer.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-echo
-echo "updating mirrorlist before doing package updates"
-echo
-sudo reflector -c Sweden,Germany,Denmark,Finland -l 30 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose # update mirror list
-
-echo
-echo "------------------"
-echo "mirrorlist updated"
-echo "------------------"
-echo
+read -rp "make sure to look at https://archlinux.org/news/ in case anything has broken. Do you want to continue with the updating? [y/n] " answer
+[[ "$answer" == "y" ]] || exit 0
 
 # ---------- go through PKGBUILD diff of each AUR package that has an update
 
@@ -42,8 +34,20 @@ fi
 # ----------
 
 echo
-read -rp "make sure to look at https://archlinux.org/news/ in case anything has broken. Do you want to continue with the updating? [y/n] " answer
-[[ "$answer" == "y" ]] || exit 0
+echo "updating mirrorlist before doing package updates..."
+echo
+
+sudo reflector -c Sweden,Germany,Denmark,Finland -l 30 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose # update mirror list
+
+echo
+echo "------------------"
+echo "mirrorlist updated"
+echo "------------------"
+echo
+
+echo
+echo "updating packages with yay..."
+echo
 
 yay -Syu --noconfirm # update system
 
@@ -53,8 +57,11 @@ echo "packages finished updating using yay"
 echo "------------------------------------"
 echo
 
+echo
+echo "updating flatpak packages..."
+echo
+
 flatpak update -y # update flatpak
-flatpak uninstall --unused -y # get rid of unused flatpak packages
 
 echo
 echo "------------------------"
@@ -62,8 +69,19 @@ echo "flatpak packages updated"
 echo "------------------------"
 echo
 
+echo
+echo "getting rid of unused flatpak packages..."
+echo
+
+flatpak uninstall --unused -y # get rid of unused flatpak packages
+
 orphans=$(pacman -Qdtq)
 if [ -n "$orphans" ]; then
+    
+    echo
+    echo "getting rid of orpahs with pacman..."
+    echo
+
     sudo pacman -Rns $orphans # remove orphaned packages
 
     echo
@@ -72,6 +90,10 @@ if [ -n "$orphans" ]; then
     echo "----------------------"
     echo
 fi
+
+echo
+echo "getting rid of old packages and unused cached package versions..."
+echo
 
 sudo paccache -r # only keep the last 3 versions of each package
 sudo paccache -ruk0 # remove cached versions of packages that aren't used
@@ -82,12 +104,20 @@ echo "removed unused pacman caches"
 echo "----------------------------"
 echo
 
+echo
+echo "getting rid of unused yay caches, making sure to keep old PKGBUILDs for this script..."
+echo
+
 find ~/.cache/yay -name '*.pkg.tar.*' -delete # clears caches whilst avoiding removing old PKGBUILDs which are used in this script
 
 echo
 echo "-----------------------------------"
 echo "finished cleaning unused yay caches"
 echo "-----------------------------------"
+echo
+
+echo
+echo "getting rid of old journalctl logs..."
 echo
 
 sudo journalctl --vacuum-time=10d # remove logs older than 10 days
