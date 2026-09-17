d65feffac7e0431f6c941a6f36fbb63e8f364be8
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-09-17 16:03:15 +0200

    display news using paru

--- archmaintainer.sh
+++ archmaintainer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-read -rp "make sure to look at https://archlinux.org/news/ in case anything has broken. Do you want to continue with the updating? [y/n] " answer
+paru -Pww
+read -rp "Do you want to continue updating? [y/n] " answer
 [[ "$answer" == "y" ]] || exit 0
 
 echo
