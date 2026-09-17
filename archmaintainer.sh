07bf3253be7825087b658326d589d507368fa3e9
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-09-17 16:05:03 +0200

    shortened and concentrated reflector to Sweden

--- archmaintainer.sh
+++ archmaintainer.sh
@@ -8,7 +8,7 @@ echo
 echo "updating mirrorlist before doing package updates..."
 echo
 
-sudo reflector -c Sweden,Germany,Denmark,Finland -l 30 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose # update mirror list
+sudo reflector -c Sweden -l 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist --verbose # update mirror list
 
 echo
 echo "------------------"
